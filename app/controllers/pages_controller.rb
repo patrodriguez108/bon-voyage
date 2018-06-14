3a127fd25dd5a294a80ce3a2d496506570ed0133
Patrick Rodriguez <pat.rodriguez108@gmail.com>
2018-06-13 22:34:02 -0500

    Add mailer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,12 @@ class PagesController < ApplicationController
 
 	def contact
 	end
+
+	def send_contact
+		@client = params[:contact]
+
+		ContactMailer.contact_bon_voyage(@client)
+
+		redirect_to contact_path
+	end
 end
