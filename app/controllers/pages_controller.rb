21511ad3054a2fc5de4df593b45310e2cefee11e
Patrick Rodriguez <pat.rodriguez108@gmail.com>
2018-09-19 10:35:29 -0500

    Remove contact page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,5 @@
 class PagesController < ApplicationController
+
 	def about
-	end
-
-	def contact
-	end
-
-	def send_contact
-		@client = params[:contact]
-
-		ContactMailer.contact_bon_voyage(@client)
-
-		redirect_to contact_path
 	end
 end
