286ebe7b2bc2e5efe5fbec797ef30956509d8639
Patrick Rodriguez <pat.rodriguez108@gmail.com>
2018-12-03 13:56:33 -0600

    Add img attributes to Piece

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -2,4 +2,8 @@ class MediaController < ApplicationController
 
   def index
   end
+
+  def show
+    @medium = Medium.find(params[:id])
+  end
 end
--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,3 +1,11 @@
 class Piece < ApplicationRecord
   belongs_to :medium
+
+  has_attached_file :img, styles: {
+    thumb: '100x100>',
+    square: '200x200#',
+    medium: '300x300>'
+  }
+
+  validates_attachment_content_type :img, :content_type => /\Aimage\/.*\Z/
 end
