59e5440c8ad346858e4f84eb9568bd612c158aa2
Patrick Rodriguez <pat.rodriguez108@gmail.com>
2018-12-04 15:17:15 -0600

    Add form to upload pieces

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -2,4 +2,22 @@ class PiecesController < ApplicationController
 
   def index
   end
+
+  def new
+    @piece = Piece.new
+  end
+
+  def create
+    @piece = Piece.new(piece_params)
+
+    if @piece.save
+      p @piece
+    end
+  end
+
+  private
+
+  def piece_params
+    params.require(:piece).permit(:name, :img)
+  end
 end
